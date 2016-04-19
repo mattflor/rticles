fc780f6fe702ea8bf38d79fc778273913a00d662
Matthias Flor <matthias.flor@bfr.bund.de>
2016-04-19 15:03:59 +0200

    Renamed plosone --> plos; yaml insertion working.

---
+++ R/plos_article.R
@@ -0,0 +1,29 @@
+#' Elsevier journal format.
+#'
+#' Format for creating submissions to Elsevier journals. Adapted from
+#' \href{http://journals.plos.org/plosone/s/latex}{http://journals.plos.org/plosone/s/latex}.
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("MyArticle.Rmd", template = "plos_article", package = "rticles")
+#' }
+#'
+#' @export
+plos_article <- function(...,
+                         keep_tex = TRUE,
+                         number_sections = FALSE,
+                         md_extensions = c("-autolink_bare_uris")) {
+  inherit_pdf_document(...,
+                       template = find_resource("plos_article", "template.tex"),
+                       keep_tex = keep_tex,
+                       number_sections = number_sections,
+                       md_extensions = md_extensions)
+}
