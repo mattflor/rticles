54a7db328f91703c4c0a20a7a5eb1de6dd1429e0
Matthias Flor <matthias.flor@bfr.bund.de>
2016-04-21 16:56:48 +0200

    Ugly but functional work around for citations.

--- R/plos_article.R
+++ R/plos_article.R
@@ -20,14 +20,22 @@
 plos_article <- function(...,
                          keep_tex = TRUE,
                          md_extensions = c("-autolink_bare_uris")) {
-#   inherit_pdf_document(...,
-#                        template = find_resource("plos_article", "template.tex"),
-#                        keep_tex = keep_tex,
-#                        md_extensions = md_extensions)
-  pdf_document_format(...,
-                      keep_tex = keep_tex,
-                      md_extensions = md_extensions,
-                      format = "plos_article",
-                      template = "template.tex",
-                      csl = "plos.csl")
+  inherit_pdf_document(...,
+                       template = find_resource("plos_article", "template.tex"),
+                       keep_tex = keep_tex,
+                       md_extensions = md_extensions)
+}
+
+
+#' @export
+plos_pdf <- function(rmd_file, open_viewer = TRUE) {
+  base_file <- strsplit(rmd_file, split = "\\.")[[1]][1]
+  tex_file <- paste0(base_file, ".tex")
+  pdf_file <- paste0(base_file, ".pdf")
+  rmarkdown::render(rmd_file, output_file = tex_file)
+  system(paste("pdflatex", tex_file))
+  system(paste("bibtex", base_file))
+  system(paste("pdflatex", tex_file))
+  system(paste("pdflatex", tex_file))
+  if (open_viewer) system(paste("open", pdf_file))
 }
