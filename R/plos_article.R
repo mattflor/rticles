8c11d18805b80de77e0b00a6c692ef1331ef6a60
Matthias Flor <matthias.flor@bfr.bund.de>
2016-04-22 10:39:01 +0200

    New function make_plos_article.

--- R/plos_article.R
+++ R/plos_article.R
@@ -1,6 +1,6 @@
-#' Elsevier journal format.
+#' PLOS journal format.
 #'
-#' Format for creating submissions to Elsevier journals. Adapted from
+#' Format for creating submissions to PLOS journals. Adapted from
 #' \href{http://journals.plos.org/plosone/s/latex}{http://journals.plos.org/plosone/s/latex}.
 #'
 #' @inheritParams rmarkdown::pdf_document
@@ -27,15 +27,33 @@ plos_article <- function(...,
 }
 
 
+#' Make a PLOS journal PDF
+#'
 #' @export
-plos_pdf <- function(rmd_file, open_viewer = TRUE) {
+make_plos_article <- function(rmd_file, open_viewer = TRUE) {
+  # filenames
   base_file <- strsplit(rmd_file, split = "\\.")[[1]][1]
   tex_file <- paste0(base_file, ".tex")
   pdf_file <- paste0(base_file, ".pdf")
+
+  # knit and convert .Rmd to .tex
   rmarkdown::render(rmd_file, output_file = tex_file)
-  system(paste("pdflatex", tex_file))
-  system(paste("bibtex", base_file))
-  system(paste("pdflatex", tex_file))
-  system(paste("pdflatex", tex_file))
+
+  # latex commands
+  pdflatex_cmd <- paste("pdflatex", tex_file)
+  bibtex_cmd <- paste("bibtex", base_file)
+
+  # create .pdf by running
+  # pdflatex, the bibtex, then pdflatex twice
+  message("pdflatex run #1 ...")
+  system(pdflatex_cmd)
+  message("bibtex run ...")
+  system(bibtex_cmd)
+  message("pdflatex run #2 ...")
+  system(pdflatex_cmd)
+  message("pdflatex run #3 ...")
+  system(pdflatex_cmd)
+
+  # open .pdf
   if (open_viewer) system(paste("open", pdf_file))
 }
